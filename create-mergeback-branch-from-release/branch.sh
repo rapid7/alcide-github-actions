7d065605ba54a536203e80f0f3f782f22e2811ea
Moran Rotman <91007521+mrotman-r7@users.noreply.github.com>
2021-11-30 13:26:38 +0200

    added new action for auto-mergebacks

---
+++ create-mergeback-branch-from-release/branch.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+source $(dirname "$0")/common.sh
+
+VERSION_NAME=$1
+GIT_USER_NAME=$2
+GIT_USER_EMAIL=$3
+GITHUB_TOKEN=$4
+
+function configureNewBranch()
+{
+    local sourceBranch=$1
+    local shortBranchName=${sourceBranch#remotes/origin/}
+    echo "#########################################"
+    echo "Branching from source: $sourceBranch"
+    echo "#########################################"
+    date_merge=$(date +%Y%m%d)
+    echo $date_merge
+    branchReleaseName="mergeback__$shortBranchName-$date_merge"
+    echo "gh auth status"
+    gh auth status
+    git checkout $sourceBranch
+    echo "Checkout branch: $sourceBranch"
+    git branch $branchReleaseName || error "can't checkout branch: $branchReleaseName, source: $sourceBranch"
+    git checkout $branchReleaseName
+    echo "finish branching project, pushing to repo"
+    git push -u origin $branchReleaseName || error "can't push branch: $branchReleaseName to remote"
+    echo "Creating pull request with reviewers:"
+    gh pr create --title $branchReleaseName --body "automatically created because changes detected" --reviewer mrotman-r7 --head $branchReleaseName --base ENG-108111_automate_mergebacks
+    #echo "Creating auto merge for pull request"
+    #gh pr merge $branchReleaseName --auto -m
+    echo "finished creating pull request"
+    echo "##########################"
+
+}
+
+function gitConfig()
+{
+    echo "Configuring git creds"
+    git config --global user.name $GIT_USER_NAME
+    git config --global user.email $GIT_USER_EMAIL
+    echo "$GITHUB_TOKEN" > .githubtoken
+    gh auth login --with-token < .githubtoken
+}
+
+echo "VERSION_NAME: $VERSION_NAME"
+echo "GIT_USER_NAME: $GIT_USER_NAME"
+echo "GIT_USER_EMAIL: $GIT_USER_EMAIL"
+
+gitConfig
+echo "Checking if development branch is up to date"
+branchDiff=(git diff $VERSION_NAME..main)
+echo "contents of $branchDiff"
+if [ -z "${VERSION_NAME}" ]; then
+    echo "COMMIT_SHA is empty"
+elif [ ! -z "$branchDiff" ]; then
+  configureNewBranch ${VERSION_NAME}
+else
+    echo "Dev branch up to date."
+fi
---
+++ create-mergeback-branch-from-release/get_latest_release.py
@@ -0,0 +1,17 @@
+max_version = (0, 0, 0, 0)
+with open("branches") as list_of_branches:
+    for branch in list_of_branches:
+        branch = branch.strip()
+        words = branch.split('/')
+        version = (words[-1:])
+        semantic = ''.join(version)
+        semantic = semantic.split('.')
+        semantic = list(map(int, semantic))
+        semantic_tuple = tuple(semantic)
+        #print(semantic_tuple)
+        if semantic_tuple > max_version:
+            max_version = semantic_tuple
+            branch_name = branch
+
+#print(max_version)
+print(branch_name)
